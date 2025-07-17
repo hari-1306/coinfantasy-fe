2f0f0e7b7be9fa45b263afba58b6e4f9a6546a1a
Hari <hariharan.as@randomwalk.ai>
2025-07-17 16:37:34 +0530

    chore: modify backend endpoint url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	let isLoading = $state(false);
 	let chatContainer: HTMLElement | null = $state(null);
 
-	const BACKEND_URL = 'http://127.0.0.1:8000/chat';
+	const BACKEND_URL = 'https://coinfantasy-be.onrender.com/chat';
 
 	onMount(() => {
 		messages = [
